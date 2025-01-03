b56ece355d696865c2082cefefee1bfe275e4676
Mohammad Dilkash <128279053+Dilkashshaikh92@users.noreply.github.com>
2025-01-03 23:15:00 +0530

    Set up Venv environment for Python

---
+++ Lession-1/operators_example.py
@@ -0,0 +1,24 @@
+# Arithmetic Operators
+x = 10
+y = 5
+
+   
+print("Addition:", x +  y) #  Addition
+
+# Subtraction
+print("Subtraction:", x -  y)
+
+      # Multiplication
+ print("Multiplication:",  x * y)
+
+ # Division
+print("Division:", x / y)
+
+   # Comparison Operators
+  print("Is x greater than y?", x > y)
+print("Is x equal to y?", x == y)
+
+  # Logical Operators
+is_active = True
+is_logged_in = False
+print("User is active and logged in:", is_active and is_logged_in)
---
+++ Lession-1/variables_example.py
@@ -0,0 +1,12 @@
+# Defining variables of different types
+integer_var = 20  # My Age 
+ float_var = 22.5  # Age with decimal ( example, height or weight)
+string_var = "Mohammad Dilkash"  # My name (Mohammad Dilkash)
+boolean_var = True  # True value
+
+# Printing variable values
+print("Integer Variable (My Age):", integer_var)
+print("Float Variable (My Height):", float_var)
+print("String Variable (Name):", string_var)
+print("Boolean Variable (Is active):", boolean_var)
+
